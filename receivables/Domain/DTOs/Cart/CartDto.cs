68a8367b784e3f0bf16795d794449241186081c1
AnaJulia-Ps <anajulia5897@hotmail.com>
2025-11-26 10:44:04 -0300

    ajuste

--- receivables/Domain/DTOs/Cart/CartDto.cs
+++ receivables/Domain/DTOs/Cart/CartDto.cs
@@ -8,12 +8,4 @@ namespace receivables.Domain.DTOs.Cart
         public int TotalItems { get; set; }
         public decimal TotalGross { get; set; }
     }
-
-    public class CartInvoiceDto
-    {
-        public Guid Id { get; set; }
-        public string Number { get; set; } = string.Empty;
-        public decimal Amount { get; set; }
-        public DateTime DueDate { get; set; }
-    }
 }
---
+++ receivables/Domain/DTOs/Cart/CartInvoiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace receivables.Domain.DTOs.Cart
+{
+    public class CartInvoiceDto
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
